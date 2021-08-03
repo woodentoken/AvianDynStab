4499d8aa8b990ffd090af3fdf9040860194ac642
charvey23 <harveyca@umich.edu>
2021-08-03 18:22:35 -0400

    only calc time series for desired inputs

--- run_script.py
+++ run_script.py
@@ -18,9 +18,8 @@ c_max = 0.2861011  # wing root chord
 coef_data = pd.read_csv('/Users/christinaharvey/Google Drive/DoctoralThesis/Chapter3_DynamicStability'
                         '/2021_07_29_coefficients.csv')
 a_test = [-2, 0, 2, 4, 6]
-elbow_test = np.arange(80, 170, 5)
-manus_test = np.arange(100, 185, 5)
-
+elbow_test = np.arange(80, 170, 1)
+manus_test = np.arange(100, 185, 1)
 today = date.today()
 date_adj = today.strftime("%Y_%m_%d")
 
@@ -52,22 +51,33 @@ for i in a_test:
             A = dynfn.solve_linsys(m, Iyy, rho, S_max, c_max, elbow, manus, alpha_0, U_0,
                                    theta_0, coef_data, del_x, del_z)
 
-            # Step 3: Extract the time response of the system wrt d_alp
-            out = dynfn.calc_res_dalp(5, 60, 6000, A)
+            # initialize to not compute time series unless specified
+            compute = False
 
-            filename = "outputdata/" + \
-                       date_adj + "_elbow" + str(elbow) + "_manus" + str(manus) + \
-                       "_alpha" + str(alpha_0) + "dalp.csv"
-            np.savetxt(filename, out, delimiter=",")
+            # Only grab time series of the following configurations
+            if e == 90 and w == 120:
+                compute = True
+            if e == 120 and w == 120:
+                compute = True
+            if e == 120 and w == 157:
+                compute = True
+            if compute:
+                # Step 3: Extract the time response of the system wrt d_alp
+                out = dynfn.calc_res_dalp(5, 60, 6000, A)
 
-            # Step 4: Extract the time response of the system wrt ramped up speed
-            B = np.asarray([0, 0.01, 0, 0])
+                filename = "outputdata/" + \
+                            date_adj + "_elbow" + str(elbow) + "_manus" + str(manus) + \
+                            "_alpha" + str(alpha_0) + "_dalp.csv"
+                np.savetxt(filename, out, delimiter=",")
 
-            ramp_end = 30  # seconds
-            out = dynfn.calc_res_uramp(60, ramp_end*200, A, B, ramp_end)
+                # Step 4: Extract the time response of the system wrt ramped up speed
+                B = np.asarray([0, 0.01, 0, 0])
 
-            filename = "outputdata/" + \
-                       date_adj + "_elbow" + str(elbow) + "_manus" + str(manus) + \
-                       "_alpha" + str(alpha_0) + "_uramp.csv"
-            np.savetxt(filename, out, delimiter=",")
+                ramp_end = 30  # seconds
+                out = dynfn.calc_res_uramp(60, ramp_end*200, A, B, ramp_end)
+
+                filename = "outputdata/" + \
+                            date_adj + "_elbow" + str(elbow) + "_manus" + str(manus) + \
+                            "_alpha" + str(alpha_0) + "_uramp.csv"
+                np.savetxt(filename, out, delimiter=",")
 
